a513eac049253e76d1e2f727c40c004e1545334c
Sergio Matamala <sergio.matamala93@gmail.com>
2024-01-12 22:32:01 -0300

    skeleton

---
+++ src/test/java/matamala/sergio/archetypes/springbootarchetype/SpringbootArchetypeApplicationTests.java
@@ -0,0 +1,13 @@
+package matamala.sergio.archetypes.springbootarchetype;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootArchetypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
